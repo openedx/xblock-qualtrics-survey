e9a162fb87e6627f3160900c2a84d0be6df8fba4
Feanil Patel <feanil@axim.org>
2026-04-16 08:23:12 -0400

    feat!: Bump version from 2.1.1 to 3.0.0
    The code is being moved to the xblocks-extras repo, this is the last version bump for this repo and it will not receive further updates.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from os import path
 from setuptools import find_packages, setup
 
 
-version = '2.1.1'
+version = '3.0.0'
 description = __doc__.strip().split('\n')[0]
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.rst')) as file_in:
